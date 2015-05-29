4c7118551b37f9818ed717805d82126e526bc6c1
samdeanefox <samdeanefox@gmail.com>
2015-05-29 01:37:37 -0700

    refactored for basic scaffolding

--- src/kafe/Kafe.java
+++ src/kafe/Kafe.java
@@ -5,68 +5,61 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.*;
 
-
 public class Kafe extends JFrame {
-	
-	public static final Kafe instance;
-	private JPanel mainPanel;
-	private JButton start;
-	private JButton exit;
-	
-	static {
-		instance = new Kafe();
-	}
-	
-	public static void main(String[] args) {
-		Kafe app = getKafe();
-	}
-	
-	public static Kafe getKafe() {
-		return instance;
-	}
+	private static final Kafe instance = new Kafe();
+	private static final int WINDOW_X = 691;
+	private static final int WINDOW_Y = 922;
+	private Dimension SCREEN_DIMENSION;
+	private JPanel	view,
+					DEFAULT_VIEW,
+					LOGIN_VIEW,
+					HOME_VIEW;
+	private JLabel logoText;
+	private JButton loginButton;
+	public static Kafe getKafe() {  return instance;  }
+	public static void main(String[] args) {  getKafe(); }
 	
 	private Kafe() {
+		//Init Stuff
 		super("Kafe");
-		//Main Menu
-		start = new JButton("Let's play");
-		exit = new JButton("Let's not");
-		start.addActionListener(new ButtonListener());
-		exit.addActionListener(new ButtonListener());
-		//Build Panel
-		mainPanel = new JPanel();
-		mainPanel.add(start);
-		mainPanel.add(exit);
-		//Build Window
-		setSize(500, 75);
+		SCREEN_DIMENSION = Toolkit.getDefaultToolkit().getScreenSize();
+		DEFAULT_VIEW = new JPanel();
+		LOGIN_VIEW = new JPanel();
+		HOME_VIEW = new JPanel();
+		logoText = new JLabel("Kafe");
+		loginButton = new JButton("Sign In");
+		
+		//Default View
+		logoText.setFont(new Font("Serif", Font.PLAIN, 72));
+		loginButton.addActionListener(new ButtonListener());
+		DEFAULT_VIEW.add(logoText);
+		DEFAULT_VIEW.add(loginButton);
+		
+		//Login View
+		
+		//Home View
+		
+		//Window position
+		setSize(WINDOW_X, WINDOW_Y);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		add(mainPanel); 
-		//Position the window in the center of the screen
-		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize(); 	// Get the size of the screen
-		int w = getSize().width; 	// Determine the new location of the window
-		int h = getSize().height;
-		int x = (dim.width-w)/2;
-		int y = (dim.height-h)/2;
-		setLocation(x, y); 	// Move the window
+		int w = getSize().width,
+			h = getSize().height,
+			x = (SCREEN_DIMENSION.width-w)/2,
+			y = (SCREEN_DIMENSION.height-h)/2;
+		setLocation(x, y);
+		
+		//Fire it up
+		view = DEFAULT_VIEW;
+		add(view);
 		setVisible(true);
 	}
 	
-	private class ButtonListener implements ActionListener
-	{
-		public void actionPerformed(ActionEvent e)
-		{
-		//Difficulty Menu
-		if(start == e.getSource())
-		{
-			setVisible(false); //get rid of this window
-			//Difficulty2 difficulty = new Difficulty2(); //launch next window
-		}
-		
-		//If exit button is pressed
-		if(exit == e.getSource())
-		{
-			System.exit(0); //quit
-		}
+	private class ButtonListener implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			if(e.getSource() == loginButton) {
+				setVisible(false);
+				System.exit(0);
+			}
 		}
 	}
-
 }
